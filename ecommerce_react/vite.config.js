d3ea9528a59d8ade72ccb19ec0b2592818bbdee9
cruiz386 <cruiz@teco.com.ar>
2024-08-06 20:35:38 -0300

    config base vite

--- ecommerce_react/vite.config.js
+++ ecommerce_react/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: 'https://cruiz386.github.io/CreaTuLanding_RuizCristian/',
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-  base: 'https://cruiz386.github.io/CreaTuLanding_RuizCristian/',
-})
+  base: '/CreaTuLanding_RuizCristian/',
+});
