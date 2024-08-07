2b8f1b2c4748dd46aac1eb99c5a47ab4c8740faf
cruiz386 <cruiz@teco.com.ar>
2024-08-06 22:20:10 -0300

    modificaciones - rutas item

--- ecommerce_react/src/components/Item.jsx
+++ ecommerce_react/src/components/Item.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Item = ({ id, image, name, price, description, category, stock }) => {
     
-    const cleanImageUrl = image.startsWith('.') ? image.substring(1) : image;
+    const imageUrl = image; 
 
     return (
         <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100 align-items-center justify-content-center mb-4">
                 <img
-                    src={cleanImageUrl}
+                    src={imageUrl}
                     className="card-img-top m-2"
                     alt={name}
                     style={{ width: '15rem', height: '200px', maxWidth: '200px', maxHeight: '250px', alignSelf: 'center' }}
@@ -32,3 +32,4 @@ const Item = ({ id, image, name, price, description, category, stock }) => {
 };
 
 export default Item;
+
