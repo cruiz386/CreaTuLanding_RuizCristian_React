323d2a510671dbb74841775e9b24b16022134d17
cruiz386 <cruiz@teco.com.ar>
2024-08-06 21:49:01 -0300

    modificaciones - rutas item detail

--- ecommerce_react/src/components/ItemDetail.jsx
+++ ecommerce_react/src/components/ItemDetail.jsx
@@ -9,7 +9,7 @@ const ItemDetail = () => {
     const { itemId } = useParams();
     const [product, setProduct] = useState(null);
     const [quantity, setQuantity] = useState(1);
-   
+    const baseUrl = "https://cruiz386.github.io/CreaTuLanding_RuizCristian/assets/";
 
     useEffect(() => {
         if (products.length > 0) {
@@ -47,7 +47,7 @@ const ItemDetail = () => {
         <div className="col-lg-4 col-md-6 mb-4">
             <div className="card h-100 align-items-center justify-content-center mb-4">
                 <img
-                    src={product.image}
+                    src={product.image.startsWith('http') ? product.image : `${baseUrl}${product.image}`} 
                     className="card-img-top m-2"
                     alt={product.name}
                     style={{ width: '15rem', height: '200px', maxWidth: '200px', maxHeight: '250px', alignSelf: 'center' }}
